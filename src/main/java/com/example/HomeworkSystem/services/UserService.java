0408b728c9215b26683d8a15349e5b17101c9a6e
Saptaparna10 <saptaparna1991@gmail.com>
2018-05-13 20:22:43 -0400

    created server side user service create user

--- src/main/java/com/example/HomeworkSystem/services/UserService.java
+++ src/main/java/com/example/HomeworkSystem/services/UserService.java
@@ -1,5 +1,30 @@
 package com.example.HomeworkSystem.services;
 
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.HomeworkSystem.repositories.UserRepository;
+
+import com.example.HomeworkSystem.models.User;
+
+@RestController
 public class UserService {
+    @Autowired
+    UserRepository repository;
+
+    @PostMapping("/api/user")
+    public User createUser(@RequestBody User user) {
+        return repository.save(user);
+    }
 
 }
